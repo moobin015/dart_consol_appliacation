1372a77467880bf71fcb141eb7fe48c1ea64d863
moobin <moobinislam3@gmail.com>
2024-12-28 12:23:20 +0600

    pus

--- bin/dart_consol_appliacation.dart
+++ bin/dart_consol_appliacation.dart
@@ -1,5 +1,4 @@
-import 'package:dart_consol_appliacation/dart_consol_appliacation.dart' as dart_consol_appliacation;
-
-void main(List<String> arguments) {
-  print('Hello world: ${dart_consol_appliacation.calculate()}!');
+void main() {
+  String name = 'This is Dart Programming';
+  print(name);
 }
