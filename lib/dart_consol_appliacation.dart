d3963589f36b54246d848e5f8b2286253b604115
moobin <moobinislam3@gmail.com>
2024-12-28 12:46:47 +0600

    2nd commit

--- lib/dart_consol_appliacation.dart
+++ lib/dart_consol_appliacation.dart
@@ -1,3 +1,23 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+
+void main() {
+  String? name;
+  int? age;
+  print(name);
+  print(age);
+
+  print(age);
+  print('Name is moobin');
+  stdout.writeln('1.Enater your first number');
+  String valu1 = stdin.readLineSync()!;
+  int one1 = int.parse(valu1);
+
+  stdout.writeln('1.Enater your first number');
+  String valu2 = stdin.readLineSync()!;
+  int tow = int.parse(valu2);
+
+  int result = one1 + tow;
+  print(result);
+  print(result);
+
 }
--- test/dart_consol_appliacation_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_consol_appliacation/dart_consol_appliacation.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
