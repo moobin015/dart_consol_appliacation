57a84c25b91bb7ba3f9919e4e9541537a3e4c53e
moobin <moobinislam3@gmail.com>
2024-12-28 12:19:23 +0600

    firs commit

---
+++ bin/dart_consol_appliacation.dart
@@ -0,0 +1,5 @@
+import 'package:dart_consol_appliacation/dart_consol_appliacation.dart' as dart_consol_appliacation;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_consol_appliacation.calculate()}!');
+}
